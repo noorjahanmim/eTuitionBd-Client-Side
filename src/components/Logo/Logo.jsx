0840f331e7366daa2005132d8a227c7cea01a7aa
noorjahanmim <noorjahanmim732@gmail.com>
2025-12-20 15:10:57 +0600

    logo change

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -10,7 +10,7 @@ const Logo = () => {
         alt="eTuitionBD logo"
         className="w-12 h-12 rounded-full  shadow"
       />
-      <h3 className="bg-gradient-to-r from-primary via-indigo-500 to-primary text-xl md:text-2xl font-bold tracking-wide">
+      <h3 className="text-2xl md:text-3xl font-extrabold tracking-wide bg-gradient-to-r from-primary via-indigo-500 to-primary bg-clip-text text-transparent text-xl md:text-2xl font-bold tracking-wide">
         eTuitionBD
       </h3>
     </Link>

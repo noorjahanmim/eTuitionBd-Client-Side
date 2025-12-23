605ade1690f3898ab77dcaa411e5fcba8cc73a53
noorjahanmim <noorjahanmim732@gmail.com>
2025-12-23 15:32:32 +0600

    file modified

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Footer from '../pages/Shared/Footer/Footer';
-import Navbar from '../pages/Shared/NavBar/NavBar';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
+import Navbar from "../pages/Shared/NavBar/NavBar";
 
 const RootLayout = () => {
   return (
-
-
-    <div className=" flex flex-col min-h-screen font-sans">
+    <div className="flex flex-col min-h-screen font-sans bg-gray-50">
+      
       <Navbar />
-      <main className="flex-grow container mx-auto px-4 py-6">
-        <Outlet />
+
+      
+      <main className="flex-grow">
+        <div className="max-w-7xl mx-auto px-4 py-8">
+          <Outlet />
+        </div>
       </main>
+
+      
       <Footer />
     </div>
-
-
   );
 };
 
